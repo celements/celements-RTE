2bb736271440215f3e9d2047d66b60416177a717
Fabian Pichler <fabian.pichler@synventis.com>
2023-01-05 22:42:01 +0100

    update lazy loader version

--- RichText-web-module/src/main/webapp/resources/celRteJS/tinyMCE6/loadTinyMCE-async.mjs
+++ RichText-web-module/src/main/webapp/resources/celRteJS/tinyMCE6/loadTinyMCE-async.mjs
@@ -19,7 +19,7 @@
  */
 
 
-import "../celDynJS/DynamicLoader/celLazyLoader.mjs?version=202301042226";
+import "../celDynJS/DynamicLoader/celLazyLoader.mjs?version=202301053341";
 import { CelUploadHandler }
   from "../celDynJS/upload/fileUpload.mjs?version=202212020804";
 import { CelFilePicker }
