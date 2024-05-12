c462c82660125ca1922af97d4fb24759b1f176be
Fabian Pichler <fabian.pichler@synventis.com>
2024-05-12 21:12:24 +0200

    resolve conflict

--- RichText-component/src/main/java/com/celements/rteConfig/RTEConfig.java
+++ RichText-component/src/main/java/com/celements/rteConfig/RTEConfig.java
@@ -37,14 +37,9 @@ import javax.validation.constraints.NotNull;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-<<<<<<< HEAD
+import org.springframework.context.annotation.Primary;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
-=======
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponents;
->>>>>>> c7d0d26 (add primary annotation)
 import org.xwiki.configuration.ConfigurationSource;
 import org.xwiki.model.reference.ClassReference;
 import org.xwiki.model.reference.DocumentReference;
