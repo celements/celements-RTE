2ee27fd50af3e71e543f967cb6f39ceedeb3efdc
Chantal Pichler <chantal.pichler@synventis.com>
2026-03-30 15:49:45 +0200

    remove logging

--- RichText-web-module/src/main/webapp/resources/celRTE/4.9.11/plugins/celimage/js/imagepicker.js
+++ RichText-web-module/src/main/webapp/resources/celRTE/4.9.11/plugins/celimage/js/imagepicker.js
@@ -263,7 +263,6 @@ var loadAttachmentListCallback = function(attList, insertBottom, duplicateCheck,
         'src' : thmbImgSrc,
         'loading' : 'lazy',
       });
-      consle.log('img attributes: ', imgThmb, imgThmb.getAttribute('loading'));
       var imgContainerDiv = new Element('div', {
         'class' : cssClasses
       }).update(imgThmb);
